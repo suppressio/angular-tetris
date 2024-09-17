65f03e9d13e17cdd4a008f08f1d8458c2ef328fd
suppressio <daniele@suppressio.net>
2024-09-17 19:00:06 +0200

    wallkick on rotate

--- src/app/models/game.model.ts
+++ src/app/models/game.model.ts
@@ -1,4 +1,4 @@
-export const PIECES = {
+export const TERAMINOS = {
     I: [
         [false, false, false, false],
         [true, true, true, true],
@@ -6,7 +6,7 @@ export const PIECES = {
         [false, false, false, false],
     ],
     L: [
-        [true, false, false], 
+        [true, false, false],
         [true, true, true],
         [false, false, false],
     ],
@@ -32,18 +32,24 @@ export const PIECES = {
     T: [
         [false, false, false],
         [true, true, true],
-        [false, true, false], 
+        [false, true, false],
     ],
 }
 
-export type PiecesIdx = keyof typeof PIECES;
+export type TeraminoKeys = keyof typeof TERAMINOS;
 
+export interface Teramino { 
+  piece: number[][], 
+  rotation: keyof typeof Rotations,
+  type: keyof typeof TERAMINOS,
+}
+ 
 export enum Moves {
     DOWN = "d",
-    UP = "u",
     LEFT = "l",
     RIGHT = "r",
     ROTATE = "t",
+    SCROLL = "s",
 }
 
 export enum GameStates {
@@ -53,4 +59,50 @@ export enum GameStates {
     GAMEOVER,
 }
 
-export interface Coords { x: number, y: number } 
+export interface Coords { x: number, y: number }
+
+export enum Rotations {
+    R_0 = "0",
+    R_R = "R", 
+    R_2 = "2",
+    R_L = "L",
+}
+
+export interface WallKick {
+    "0R": number[][];
+    "R0": number[][];
+    "R2": number[][];
+    "2R": number[][];
+    "2L": number[][];
+    "L2": number[][];
+    "L0": number[][];
+    "0L": number[][];
+}
+
+export const WALL_KICK_JLSTZ: WallKick = {
+    "0R": [[-1, 0], [-1,  1], [0, -2], [-1, -2]],
+    "R0": [[ 1, 0], [ 1, -1], [0,  2], [ 1,  2]],
+    "R2": [[ 1, 0], [ 1, -1], [0,  2], [ 1,  2]],
+    "2R": [[-1, 0], [-1,  1], [0, -2], [-1, -2]],
+    "2L": [[ 1, 0], [ 1,  1], [0, -2], [ 1, -2]],
+    "L2": [[-1, 0], [-1, -1], [0,  2], [-1,  2]],
+    "L0": [[-1, 0], [-1, -1], [0,  2], [-1,  2]],
+    "0L": [[ 1, 0], [ 1,  1], [0, -2], [ 1, -2]],
+}
+
+export const WALL_KICK_I: WallKick = {
+    "0R": [[-2, 0], [ 1, 0], [-2, -1], [ 1,  2]],
+    "R0": [[ 2, 0], [-1, 0], [ 2,  1], [-1, -2]],
+    "R2": [[-1, 0], [ 2, 0], [-1,  2], [ 2, -1]],
+    "2R": [[ 1, 0], [-2, 0], [ 1, -2], [-2,  1]],
+    "2L": [[ 2, 0], [-1, 0], [ 2,  1], [-1, -2]],
+    "L2": [[-2, 0], [ 1, 0], [-2, -1], [ 1,  2]],
+    "L0": [[ 1, 0], [-2, 0], [ 1, -2], [-2,  1]],
+    "0L": [[-1, 0], [ 2, 0], [-1,  2], [ 2, -1]],
+};
+
+export const getWallKickKey = (
+    from: keyof typeof Rotations, 
+    to: keyof typeof Rotations
+): keyof WallKick => 
+    `${Rotations[from]}${Rotations[to]}` as keyof WallKick ;
